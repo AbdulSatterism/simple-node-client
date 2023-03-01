4611a3f0318685c69e7fedd04089e17a88602910
Md. Abdul Satter <116937243+AbdulSatterism@users.noreply.github.com>
2023-03-01 15:45:53 +0600

    simmple node client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email }
+    console.log(user);
+    fetch('http://localhost:5000/users', {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        const newUsers = [...users, data];
+        setUsers(newUsers)
+      })
+      .catch(error => console.log(error))
+    form.reset()
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" id="" placeholder='name' />
+        <br />
+        <input type="email" name="email" id="" placeholder='email' />
+        <br />
+        <button type="submit">Add user</button>
+      </form>
+
+      <h1>User :{users.length}</h1>
+      <div>
+        {
+          users.map(user => <p key={user.id}>{user.name} {user.email}</p>)
+        }
+      </div>
     </div>
   );
 }
